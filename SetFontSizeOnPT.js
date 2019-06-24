c17bce5545677340a0fc77ff34975f056609792e
Владимир Трунов <vetrunov@chsu.ru>
2019-06-24 12:35:43 +0300

    setFontSizeOlyOneContainer GetParentParagraph

--- SetFontSizeOnPT.js
+++ SetFontSizeOnPT.js
@@ -1,11 +1,18 @@
 function SetFontSizeOnPt(FontSize){
   var selection= window.getSelection().getRangeAt(0);
   if(selection.startContainer!=selection.endContainer){
+    if(GetParentParagraph(selection.startContainer.parentElement)!=GetParentParagraph(selection.endContainer.parentElement)){
+
+    }else{
+      var elem = selection.startContainer.parentElement;
+
+    }
 
   } else {
     etFontSizeOlyOneContainer(selection.startContainer, fontSize, selection.startOffset, selection.endOffset);
   }
 }
+
 
 function setFontSizeOlyOneContainer(element, fontSize, startOffset, endOffset){
   var osts =document.createTextNode(element.textContent.substring(0,startOffset));
@@ -16,3 +23,23 @@ function setFontSizeOlyOneContainer(element, fontSize, startOffset, endOffset){
   element.parentElement.appendChild(spst);
   element.parentElement.appendChild(oste);
 }//for text in only one container
+
+function GetParentParagraph(element){
+  var res =element;
+  if(element.tagName){
+    if(element.tagName!='P'){
+      res = GetParentParagraph(element.parentElement);
+    }
+  }else{
+    res = GetParentParagraph(element.parentElement);
+  } 
+  return res;
+}
+
+function setFontSizeStart(elementE, fontSize, startOffset){
+  if(startOffset==0){
+
+  }else{
+
+  }
+}
