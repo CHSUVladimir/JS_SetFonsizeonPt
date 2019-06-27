18e534feb89c4f3810d35c6acfee3d3f1bed4610
Владимир Трунов <vetrunov@chsu.ru>
2019-06-27 11:43:25 +0300

    work? but create many span elements

--- SetFontSizeOnPT.js
+++ SetFontSizeOnPT.js
@@ -1,46 +1,52 @@
 function SetFontSizeOnPt(fontSize){
   var selection= window.getSelection().getRangeAt(0);
-  if(selection.startContainer!=selection.endContainer){
-    if(GetParentParagraph(selection.startContainer.parentElement)!=GetParentParagraph(selection.endContainer.parentElement)){
-
+  var tcon = getTextContainersInRange(selection);
+  if(tcon.length>0){
+    if(tcon.length==1){
+       setFontSizeOnlyOneContainer(tcon[0], fontSize, selection.startOffset, selection.endOffset);
     }else{
-       //console.log(selection.startContainer);
-      var elem = selection.startContainer.parentElement;
-      setFontSize(elem, fontSize, selection.startOffset, null);
-      //console.log(elem);
-      elem = elem.nextSibling;
-      //console.log(elem);
-      while(elem!=selection.endContainer){
-        console.log(elem);
-        console.log(elem.nextSibling);
-      setFontSize(elem, fontSize, null, null);
-        if(elem.nextSibling!=null){
-          elem = elem.nextSibling;
+      for(var i=0;i<tcon.length;i++){
+        if(i==0){
+          setFontSizeOnlyOneContainer(tcon[0], fontSize, selection.startOffset, null);
+        }else if(i==tcon.length-1){
+          setFontSizeOnlyOneContainer(tcon[i], fontSize, null, selection.endOffset);
         }else{
-          console.log(elem);
-          elem=elem.parentElement;
+           setFontSizeOnlyOneContainer(tcon[i], fontSize, null, null);
         }
-       /* if(elem.nodeName!='#text'){
-          elem = elem.nextSibling;
-        }else{
-          elem=elem.parentElement;
-        }   */
 
-        if(elem.nodeName=='#text'){
-           console.log(elem.nodeName);
-           console.log(elem);
-        }
-      }
-      setFontSize(elem, fontSize, null, selection.endOffset);
-    }
-  }else{
-    setFontSizeOnlyOneContainer(selection.startContainer, fontSize, selection.startOffset, selection.endOffset);
+
   }
+    }
+  }
+
+ /* var arrR=getNodesInRange(selection);
+  if(arrR.length>1){
+    for(var i=0;i<arrR.length;i++){
+    console.log(arrR[i]);
+    if(i==0){
+      setFontSizeOnlyOneContainer(arrR[i], fontSize, selection.startOffset, null);
+    }else if(i==arrR.length-1){
+      setFontSizeOnlyOneContainer(arrR[i], fontSize, null, selection.endOffset);
+    }else{
+      setFontSizeOnlyOneContainer(arrR[i], fontSize, null, null);
+    }
+  }
+  }else if(arrR.length==1){
+    setFontSizeOnlyOneContainer(arrR[0], fontSize, selection.startOffset, null);
+  }   */
 }
 
 function setFontSizeOnlyOneContainer(element, fontSize, startOffset, endOffset){
+  var startT=0;
+  var endT =element.textContent.length;
+  if(startOffset!=null){
+    startT=startOffset;
+  }
+  if(endOffset!=null){
+    endT=endOffset;
+  }
 	var eSpan =isElementSPAN(element)||isElementSPAN(element.parentElement);
-	var allE =startOffset==0 && endOffset==element.textContent.length;
+	var allE =startT==0 && endT==element.textContent.length;
 	if(eSpan&&allE){
 		if(isElementSPAN(element)){
 			element.style.fontSize=fontSize+'pt';
@@ -49,101 +55,35 @@ function setFontSizeOnlyOneContainer(element, fontSize, startOffset, endOffset){
 		}
 	}else{
 
-		var ostn =document.createTextNode(element.textContent.substring(0,startOffset));
-		var spst =CreateSpan(element.textContent.substring(startOffset, endOffset),fontSize);
-		var ostk=document.createTextNode(element.textContent.substring(endOffset));
-		element.textContent=null;
+		var ostn =document.createTextNode(element.textContent.substring(0,startT));
+		var spst =CreateSpan(element.textContent.substring(startT, endT),fontSize);
+		var ostk=document.createTextNode(element.textContent.substring(endT));
 		if(isElementSPAN(element)){
-			element.appendChild(ostn);
-			element.appendChild(spst);
-			element.appendChild(ostk);
+      element.insertBefore(ostk,element);
+      element.insertBefore(spst,ostk);
+			element.insertBefore(ostn,spst);
 		}else{
-			element.parentElement.appendChild(ostn);
-			element.parentElement.appendChild(spst);
-			element.parentElement.appendChild(ostk);
-      element.remove();//it need thats no null text element
+       element.parentElement.insertBefore(ostk,element);
+       element.parentElement.insertBefore(spst,ostk);
+			 element.parentElement.insertBefore(ostn,spst);
+
+      try{
+      element.textContent=null;
+        if(elem.nodeName=='#text'){
+        element.remove();//it need thats no null text element
+      }
+      }catch{}
+
+
 		}
 
 	}
 }//for text in only one
 
-function setFontSize(element, fontSize, startOffset, endOffset){
-  if(element!=null){
-    var startT=0;
-  var endT =element.textContent.length;
-  if(startOffset!=null){
-    startT=startOffset;
+function RemoveAnotherSpan(element, fontSize, startOffset, endOffset){
+  if(startOffset==0&&endOffset==element.textContent){
+
   }
-  if(endOffset!=null){
-    endT=endOffset;
-  }
-  removeFontSize(element);
-  setFontSizeOnlyOneContainer(element, fontSize, startT, endT);
-  }
-
-
-}//for all elements
-
-function removeFontSize(element){
-  try{
-    if(element.children){
-      var chiS=getChildSpans(element);
-      for(var i=0;i<chiS.length;i++){
-        if(chiS[i].style.fontSize && chiS[i].style.length==1){
-          if(chiS[i].children>0||chiS[i].textContent.length>0){
-              var tC =document.createTextNode(chiS[i].textContent);
-              element.parentElement.insertBefore(tC,chiS[i]);
-              chiS[i].remove();
-          }else{
-            chiS[i].remove();
-          }
-
-        }else{
-          chiS[i].style.fontSize='';
-        }
-      }
-    }
-  }catch{}
-}
-
-function getChildSpans(element){
-  try{
-    if(element.children){
-      var res =[];
-      for(var i=0; i<element.children.length;i++){
-        if(isElementSPAN(element.children[i])){
-          res.push(element.children[i]);
-          var cha =getChildSpans(element.children[i]);
-          for(var j=0;j<cha.length;j++){
-            res.push(cha[j]);
-          }
-
-        }
-      }
-      return res;
-    }
-  }catch{}
-}
-
-function setFontSizeForChildsSpans(element){
-  try{}catch{}
-}
-
-function HasSpanElement(element){
-  var res = isElementSPAN(element);
-  if(element!=null&&element.children&&element.children.length>0){
-     for(var i=0;i<element.children.length;i++){
-       res= res||isElementSPAN(element.children[i]) || HasSpanElement(element.children[i]);
-     }
-    }
-  return res;
-}
-
-function CreateSpan(text, fs){
-  var sp=document.createElement("span");
-    sp.style.fontSize=fs+'pt';
-    sp.innerHTML=text;
-  return sp;
 }
 
 function isElementSPAN(element){
@@ -154,54 +94,58 @@ function isElementSPAN(element){
   return res;
 }
 
-function GetParentParagraph(element){
- // console.log('who find');
-  //console.log(element);
-  var res =element;
-  if(element.tagName){
-    if(element.tagName!='P'){
-      //console.log('wehre find');
-     // console.log(element.parentElement);
-      res = GetParentParagraph(element.parentElement);
-
-    }
-  }else{
-    res = GetParentParagraph(element.parentElement);
-  }
-  return res;
+function CreateSpan(text, fs){
+  var sp=document.createElement("span");
+    sp.style.fontSize=fs+'pt';
+    sp.innerHTML=text;
+  return sp;
 }
 
-function ContainsChild(parentE, child){
-  var res = parentE==child;
-  if(!res&&parentE.children){
-    for(var i=0; i<parentE.children.length;i++){
-      res=res||ContainsChild(parentE.children[i],child);
-      if(res){
-        break;
-      }
+function getNextNode(node){
+    if (node.firstChild)
+        return node.firstChild;
+    while (node)
+    {
+        if (node.nextSibling)
+            return node.nextSibling;
+        node = node.parentNode;
     }
-  }
-
-  return res;
 }
 
-function getNextElementInParent(element){
-  var res=null;
-  try{
-    for(var i=0;i<element.parentElement.children.length;i++){
-      if(element.parentElement.children[i]==element){
-        if(element.parentElement.children[i+1]!=null){
-          return element.parentElement.children[i+1];
-        }else{
-          var mayGo =GetParentParagraph(element)!=null &&GetParentParagraph(element.parentElement)!=null&&GetParentParagraph(element)==GetParentParagraph(element.parentElement);
-          if(mayGo ){
-            return getNextElementInParent(element.parentElement);
-          }else{
-            return null;
-          }
-        }
-      }
+function getNodesInRange(range){
+    var start = range.startContainer;
+    var end = range.endContainer;
+    var commonAncestor = range.commonAncestorContainer;
+    var nodes = [];
+    var node;
+
+   /* // walk parent nodes from start to common ancestor
+    for (node = start.parentNode; node; node = node.parentNode){
+       if (node == commonAncestor)
+            break;
+        nodes.push(node);
+
     }
-  }catch{}
+    nodes.reverse();*/
+
+    // walk children and siblings from start until end is found
+    for (node = start; node; node = getNextNode(node))
+    {
+        nodes.push(node);
+        if (node == end)
+            break;
+    }
+
+    return nodes;
+}
+
+function getTextContainersInRange(range){
+  var res=[];
+  var arrR=getNodesInRange(range);
+   for(var i=0;i<arrR.length;i++){
+     if(arrR[i].nodeName=='#text' && arrR[i].textContent.length>0){
+       res.push(arrR[i]);
+     }
+   }
   return res;
 }
